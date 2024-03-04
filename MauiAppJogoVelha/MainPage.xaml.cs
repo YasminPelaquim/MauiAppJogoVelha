12906c6a72e4ad9dab78b57f2569489f20b21dd7
Usuario <Usuario@CPD03-M17>
2024-03-04 15:00:34 -0300

    Avanços no jogo da velha

--- MauiAppJogoVelha/MainPage.xaml.cs
+++ MauiAppJogoVelha/MainPage.xaml.cs
@@ -2,13 +2,30 @@
 {
     public partial class MainPage : ContentPage
     {
+        string vez = "0";
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-       
+        private void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Button disparador = (Button)sender;
+
+            disparador.IsEnabled = false;
+
+            if(vez == "0")
+            {
+                disparador.Text = "0";
+                vez = "X";
+
+            } else
+            {
+                disparador.Text = "X";
+                vez = "0";
+            } // Fecha if
+        }
     } // Fecha a classe MainPage
 
-} // Fecha o NameSpace 
+} // Fecha o Namespace 
